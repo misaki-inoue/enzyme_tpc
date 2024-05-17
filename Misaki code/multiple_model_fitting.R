6cdb54ff61e85bdce7239c1aff6010698aa6dfc1
Inoue <134610121+misaki-inoue@users.noreply.github.com>
2024-05-17 14:47:57 +0100

    Updated fit&plot function, AICc heatmap

--- Misaki code/multiple_model_fitting.R
+++ Misaki code/multiple_model_fitting.R
@@ -22,11 +22,12 @@ thirty_enz <- enzyme %>%
   select(originalid, interactor1temp, originaltraitvalue)
 
 # Load functions all 8 functions from "mech_model_functions" before running multi_fit()
-# Note to self: Need to add R-squared to the output dataframe
+# It will print TPCs that do not have a fitted model or the TPCs with fitted model wiht AICc = infinity
 
-multi_fit <- function(df, model_func, model_name, num_col) {
+multi_fit <- function(df, model_func, model_name) {
   plot.new()
-  stats <- data.frame(model = character(), enzyme = character(), AICc = numeric(), BIC = numeric())
+  subplot_counter = 0
+  stats <- data.frame(model = character(), enzyme = character(), Rsquared = numeric(), AICc = numeric(), BIC = numeric())
   
   enz_id <- df %>%
     distinct(originalid)
@@ -39,9 +40,9 @@ multi_fit <- function(df, model_func, model_name, num_col) {
   
   unique_enzyme_ids <- unique(tpc_to_fit$originalid)
   num_enzymes <- length(unique_enzyme_ids)
-  num_row <- ceiling(num_enzymes/num_col)
   
-  par(mfrow = c(num_row, num_col), mar = c(2, 2, 2, 1))
+  par(mfrow = c(6, 5), mar = c(2, 2, 1, 1), oma=c(0, 0, 2, 0))
+  
   
   for (i in 1:num_enzymes) {
     enzyme_id <- unique_enzyme_ids[i]
@@ -49,9 +50,19 @@ multi_fit <- function(df, model_func, model_name, num_col) {
       select(temp, trait_value)
     
     fit <- NULL
-    fit <- model_func(enzyme_data)
+    
+    #Skip fitting if fitting function produce some sort of error
+    tryCatch({
+      fit <- model_func(enzyme_data)
+    }, error = function(e) {
+      warning(paste("Error fitting enzyme", enzyme_id, ":", e$message))
+    })
     
     if (!is.null(fit)) {
+      rss <- sum((enzyme_data$trait_value - exp(fitted(fit)))^2)
+      tss <- sum((enzyme_data$trait_value - mean(enzyme_data$trait_value))^2)
+      r_squared <- 1 - (rss/tss)
+      
       aicc_val <- AICc(fit)
       
       loglik <- logLik(fit)
@@ -60,37 +71,77 @@ multi_fit <- function(df, model_func, model_name, num_col) {
       dev <- -2*as.numeric(loglik)
       bic_val  <- dev +  p * log(n)
       
-      stats[i,] <- c(model_name, enzyme_id, aicc_val, bic_val)
+      stats[i,] <- c(model_name, enzyme_id, r_squared, aicc_val, bic_val)
+      print(paste("Fit for", enzyme_id, "is completed"))
       
-      temp_pts <- data.frame(temp = seq(min(enzyme_data$temp), max(enzyme_data$temp), 0.5))
-      preds <- augment(fit, newdata = temp_pts)
-      
-      plot(enzyme_data$temp, log(enzyme_data$trait_value), 
-           type = "p", 
-           main = enzyme_id, 
-           xlab = "Temperature", 
-           ylab = "Activity")
-      lines(preds$temp, preds$.fitted, col = "red")
-    } else {
-      warning(paste("Fit for enzyme", enzyme_id, "is NULL"))
-      stats[i,] <- c(model_name ,enzyme_id, NA, NA)
-      plot(enzyme_data$temp, log(enzyme_data$trait_value), 
-           type = "p", 
-           main = enzyme_id, 
-           xlab = "Temperature", 
-           ylab = "Activity")
+      if (aicc_val == Inf) {
+        subplot_counter <- subplot_counter + 1
+        temp_pts <- data.frame(temp = seq(min(enzyme_data$temp), max(enzyme_data$temp), 0.5))
+        preds <- augment(fit, newdata = temp_pts)
+        plot(enzyme_data$temp, log(enzyme_data$trait_value),
+             type = "p",
+             main = enzyme_id,
+             xlab = "Temperature",
+             ylab = "Activity")
+        lines(preds$temp, preds$.fitted, col = "red")
+        if (subplot_counter == 1 || subplot_counter == 30) {
+          title(main = model_name, outer = TRUE, cex.main = 2)
+        }
+      }
+      # temp_pts <- data.frame(temp = seq(min(enzyme_data$temp), max(enzyme_data$temp), 0.5))
+      # preds <- augment(fit, newdata = temp_pts)
+      # plot(enzyme_data$temp, log(enzyme_data$trait_value),
+      #      type = "p",
+      #      main = enzyme_id,
+      #      xlab = "Temperature",
+      #      ylab = "Activity")
+      # lines(preds$temp, preds$.fitted, col = "red")
+      # if (i == 59 || i == 89) {
+      #   title(main = model_name, outer = TRUE, cex.main = 2)
+      # }
     }
-    print(enzyme_id)
+    else {
+      subplot_counter <- subplot_counter + 1
+      print(paste("Fit for", enzyme_id, "is NULL"))
+      stats[i,] <- c(model_name ,enzyme_id, NA, NA, NA)
+      plot(enzyme_data$temp, log(enzyme_data$trait_value),
+           type = "p",
+           main = enzyme_id,
+           xlab = "Temperature",
+           ylab = "Activity")
+      if (subplot_counter == 1 || subplot_counter == 30) {
+        title(main = model_name, outer = TRUE, cex.main = 2)
+      }
+    }
   }
   return(stats)
-} 
+}
 
 
-vant <- multi_fit(df=thirty_enz, fit_Vant_Hoff_4_pars, "Vant_Hoff",5)
-hinshel <- multi_fit(df=thirty_enz, fit_Hinshelwood_4_pars, "Hinshelwood", 5)
-johnson <- multi_fit(df=thirty_enz, fit_Johnson_Lewin_4_pars, "Johnson_Lewin", 5)
-shape <- multi_fit(df=thirty_enz, fit_Sharpe_Schoolfield_6_pars, "Sharpe_Schoolfield", 5)
-ratkowsky <- multi_fit(thirty_enz, fit_Ross_Ratkowsky_5_pars, "Ratkowksy", 5)
-hobbs <- multi_fit(thirty_enz, fit_Hobbs_4_pars, "Hobbs", 5)
-eaar <- multi_fit(thirty_enz, fit_Enzyme_Assisted_Arrhenius_5_pars, "EAAR", 5)
-ritchie <- multi_fit(thirty_enz, fit_Ritchie_4_pars,"Ritchie", 5)
+vant <- multi_fit(df=data_to_fit, fit_Vant_Hoff_4_pars, "Vant_Hoff")
+hinshel <- multi_fit(df=data_to_fit, fit_Hinshelwood_4_pars, "Hinshelwood")
+johnson <- multi_fit(df=data_to_fit, fit_Johnson_Lewin_4_pars, "Johnson_Lewin")
+sharpe <- multi_fit(df=data_to_fit, fit_Sharpe_Schoolfield_6_pars, "Sharpe_Schoolfield")
+ratkowsky <- multi_fit(data_to_fit, fit_Ross_Ratkowsky_5_pars, "Ratkowksy")
+hobbs <- multi_fit(data_to_fit, fit_Hobbs_4_pars, "Hobbs")
+eaar <- multi_fit(data_to_fit, fit_Enzyme_Assisted_Arrhenius_5_pars, "EAAR")
+ritchie <- multi_fit(data_to_fit, fit_Ritchie_4_pars,"Ritchie")
+
+stats <- bind_rows(vant, hinshel, johnson, sharpe, ratkowsky, hobbs, eaar, ritchie)
+#write.csv(stats, "eight_models_stats_v1.csv", row.names = FALSE)
+
+stats <- read.csv("eight_models_stats_v1.csv")
+stats <- stats %>%
+  mutate(AICc = ifelse(AICc == "Inf", NA, AICc))
+stats$AICc <- as.numeric(stats$AICc)
+
+hist(stats$AICc, main = "Distribution of AICc values",
+     xlab="AICc values")
+
+ggplot(stats, aes(enzyme, model, fill= AICc)) + 
+  geom_tile() +
+  scale_fill_gradient(na.value = "grey90")+
+  theme(axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        panel.background = element_blank()) +
+  labs(y="Mechanistic Models", x="enzyme TPC")
