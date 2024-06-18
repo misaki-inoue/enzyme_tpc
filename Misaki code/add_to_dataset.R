507e8b6897a228ca186c5ebb0557c415a3d60d1c
misaki-inoue <misaki.inoue01@gmail.com>
2024-06-18 23:04:41 +0100

    Removed unused files

--- Misaki code/add_to_dataset.R
+++
@@ -1,8 +0,0 @@
-data_string <- "16.77,63.12
-25.09,95.94
-28.94,100.00
-33.29,88.28
-41.74,67.97
-50.19,59.84"
-
-test <- read.table(text = data_string, sep = ",", header = FALSE, col.names = c("Column1", "Column2"))
